3f779063079c9f3e035f13f638640b78b4ef3e89
Heriptik <theomiot@gmail.com>
2021-04-16 01:04:18 +0200

    Last commit

--- src/main/java/xyz/pepefab/ultrapixelmon/main.java
+++ src/main/java/xyz/pepefab/ultrapixelmon/main.java
@@ -14,7 +14,7 @@ public class main
 {
     public static final String MODID = "uptileschunklimit";
     public static final String NAME = "UPTilesChunkLimit";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
 
     private static Logger logger;
 
